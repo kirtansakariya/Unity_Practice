e67fb9e60dd7ab2a9eba3c6acb95a3907fb00fa5
Kirtan <kirtansakariya@gmail.com>
2017-09-11 10:52:37 -0400

    added player movement

---
+++ First_Project/Assets/Scripts/Movement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(horizontal, 0.0f, vertical);
+	}
+}
