957687f83720f345ffa9051d9c717a9887e0a631
Kirtan <kirtansakariya@gmail.com>
2017-10-06 11:00:02 -0400

    working on time limit functionality

--- First_Project/Assets/Scripts/Movement.cs
+++ First_Project/Assets/Scripts/Movement.cs
@@ -7,6 +7,8 @@ public class Movement : MonoBehaviour {
 
     public float speed;
     public Text counter;
+    public float time;
+    public GameObject cubes; //fix
     private int count = 0;
     Rigidbody rb;
 
@@ -14,6 +16,7 @@ public class Movement : MonoBehaviour {
     {
         counter.text = "Count: " + count;
         rb = GetComponent<Rigidbody>();
+        time = Time.time; //fix
     }
 
     // Update is called once per frame
@@ -21,14 +24,14 @@ public class Movement : MonoBehaviour {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
-
         rb.AddForce(movement * speed);
-
+        cubes.SetActive(true); //fix
 	}
 
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(other.gameObject);
+        //Destroy(other.gameObject);
+        other.gameObject.SetActive(false); //fix
         count++;
         counter.text = "Count: " + count;
     }
