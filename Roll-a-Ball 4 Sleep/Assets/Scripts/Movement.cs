9dbe7cdb219a5fc2a47f4dd9e0a97036bc998191
Kirtan Sakariya <kirtansakariya@gmail.com>
2017-10-29 09:05:03 -0400

    finished game

---
+++ Roll-a-Ball 4 Sleep/Assets/Scripts/Movement.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public enum CollisionType{Door, Cubes};
+
+public class Movement : MonoBehaviour {
+	public CollisionType collisionType;
+    public float speed;
+	public static int userMins;
+    public Text counter;
+	public Text timeCounter;
+    public Text win;
+	public Text incorrect1;
+	public Text incorrect2;
+	public Text incorrect3;
+	public Text question;
+	public Text answers;
+	public Text A;
+	public Text B;
+	public Text C;
+	public Text D;
+	public bool go;
+	public int respawn;
+    public float time;
+	public int hr;
+	public int min;
+	public List<CollisionType> tags;
+    public GameObject[] cubes;
+    private int count = 0;
+    Rigidbody rb;
+
+    void Start()
+    {
+        counter.text = "Count: " + count + "\nTime left: " + respawn;
+        rb = GetComponent<Rigidbody>();
+        time = Time.time;
+        cubes = GameObject.FindGameObjectsWithTag("Cubes");
+		min = (System.DateTime.Now.Hour * 60) + System.DateTime.Now.Minute;
+		if (userMins == 0)
+			userMins = 22 * 60;
+		incorrect1.enabled = false;
+		incorrect2.enabled = false;
+		incorrect3.enabled = false;
+		question.enabled = false;
+		answers.enabled = false;
+		A.enabled = false;
+		B.enabled = false;
+		C.enabled = false;
+		D.enabled = false;
+		go = true;
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
+		rb.AddForce (movement * speed);
+		if (userMins - min <= 30 || System.DateTime.Now.Hour < 8) {
+			respawn = 3;
+			win.text = "Its past your bedtime! Go to bed";
+		}
+		if (rb.transform.position.ToString() != "(0.0, 0.5, 0.0)") {
+			go = false;
+			if (count == 12) {
+				win.text = "Congrats";
+				question.enabled = true;
+				answers.enabled = true;
+				A.enabled = true;
+				B.enabled = true;
+				C.enabled = true;
+				D.enabled = true;
+			} else if (Time.time - time > respawn) {
+				time = Time.time;
+				count = 0;
+				for (int i = 0; i < cubes.Length; i++) {
+					cubes [i].SetActive (true);
+				}
+			}
+		} else if (go) {
+			time = Time.time;
+		}
+		counter.text = "Count: " + count;
+		if ((time - Time.time + respawn) > 0) {
+			string tim2 = string.Format ("{0:0.00}", time - Time.time + respawn);
+			timeCounter.text = "Time left: " + tim2;
+		}
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+		if (other.gameObject.tag == "Cubes") {
+			other.gameObject.SetActive (false);
+			count++;
+			counter.text = "Count: " + count;
+		} else if (other.gameObject.tag == "GooDoor" && count == 12) {
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		} else if (other.gameObject.tag == "PooDoor1" && count == 12) {
+			incorrect1.enabled = true;
+		} else if (other.gameObject.tag == "PooDoor2" && count == 12) {
+			incorrect2.enabled = true;
+		} else if (other.gameObject.tag == "PooDoor3" && count == 12) {
+			incorrect3.enabled = true;
+		}
+    }
+}
---
+++ Roll-a-Ball 4 Sleep/Assets/Scripts/PanelManager.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PanelManager : MonoBehaviour {
+
+	public Animator initiallyOpen;
+
+	private int m_OpenParameterId;
+	private Animator m_Open;
+	private GameObject m_PreviouslySelected;
+	public Text pm9;
+	public Text pm10;
+	public Text pm11;
+	public Text am12;
+
+	const string k_OpenTransitionName = "Open";
+	const string k_ClosedStateName = "Closed";
+
+	public void OnEnable()
+	{
+		m_OpenParameterId = Animator.StringToHash (k_OpenTransitionName);
+
+		if (initiallyOpen == null)
+			return;
+
+		OpenPanel(initiallyOpen);
+	}
+
+	public void OpenPanel (Animator anim)
+	{
+		if (m_Open == anim)
+			return;
+
+		anim.gameObject.SetActive(true);
+		var newPreviouslySelected = EventSystem.current.currentSelectedGameObject;
+
+		anim.transform.SetAsLastSibling();
+
+		CloseCurrent();
+
+		m_PreviouslySelected = newPreviouslySelected;
+
+		m_Open = anim;
+		m_Open.SetBool(m_OpenParameterId, true);
+
+		GameObject go = FindFirstEnabledSelectable(anim.gameObject);
+
+		SetSelected(go);
+	}
+
+	static GameObject FindFirstEnabledSelectable (GameObject gameObject)
+	{
+		GameObject go = null;
+		var selectables = gameObject.GetComponentsInChildren<Selectable> (true);
+		foreach (var selectable in selectables) {
+			if (selectable.IsActive () && selectable.IsInteractable ()) {
+				go = selectable.gameObject;
+				break;
+			}
+		}
+		return go;
+	}
+
+	public void CloseCurrent()
+	{
+		if (m_Open == null)
+			return;
+
+		m_Open.SetBool(m_OpenParameterId, false);
+		SetSelected(m_PreviouslySelected);
+		StartCoroutine(DisablePanelDeleyed(m_Open));
+		m_Open = null;
+	}
+
+	IEnumerator DisablePanelDeleyed(Animator anim)
+	{
+		bool closedStateReached = false;
+		bool wantToClose = true;
+		while (!closedStateReached && wantToClose)
+		{
+			if (!anim.IsInTransition(0))
+				closedStateReached = anim.GetCurrentAnimatorStateInfo(0).IsName(k_ClosedStateName);
+
+			wantToClose = !anim.GetBool(m_OpenParameterId);
+
+			yield return new WaitForEndOfFrame();
+		}
+
+		if (wantToClose)
+			anim.gameObject.SetActive(false);
+	}
+
+	private void SetSelected(GameObject go)
+	{
+		EventSystem.current.SetSelectedGameObject(go);
+	}
+
+	public void setHour (Text t) {
+		if (t.text == "9 PM")
+			Movement.userMins = (9 * 60) + (12 * 60);
+		else if (t.text == "10 PM")
+			Movement.userMins= (10 * 60) + (12 * 60);
+		else if (t.text == "11 PM")
+			Movement.userMins = (11 * 60) + (12 * 60);
+		else if (t.text == "12 AM")
+			Movement.userMins = (12 * 60) + (12 * 60);
+	}
+}
